ec90d45cf5c5aefc1394b23920ba25c3a972bfc8
BHOOMI DANGAR <bhoomi.dangar@rku.ac.in>
2026-06-25 09:02:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+      public  static void Main(string[] args)
+        {
+            //Console.Write("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.Write("Hello World!");
+            //Console.writeLine("Hello World!")
+            //Console.WriteLine("Hello World!")
+            Console.Read();
+        }
+    }
+}
---
+++ sum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class sum
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, sum;
+            Console.WriteLine("Enter the first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            Console.WriteLine("The sum of {0} and {1} is: {2}", a, b, sum);
+            Console.Read();
+        }
+
+    }
+}
